c28bb16a21e8d58d93f7d8618c57dce29c8a8931
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-18 01:08:12 +0300

    class based views for login and logout route

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/",views.register,name = "users-register"),
+    path("login/",auth_views.LoginView.as_view,name = "users-login"),
+    path("register/",auth_views.LogoutView.as_view(),name = "users-logout"),
 ]
