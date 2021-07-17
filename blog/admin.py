50b97218cf445b888e606cdc64264f661a5e829d
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-17 23:57:55 +0300

    filters for view on the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    list_filter = ("title","date_posted","author",)
+admin.site.register(Post,PostAdmin)
