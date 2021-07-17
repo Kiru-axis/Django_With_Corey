bd109b0e295813f66dc84a9590ef9e20ecf9bcd7
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-17 21:04:19 +0300

    dummy data to test db

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# dummy data
+posts = [
+    {"title": "Hacker 101",
+    "author": "John Obala",
+    "content": "There is more than what you see",
+    "date_posted": "January 23 2021"
+    },
+    {"title": "Black is powered by",
+    "author": "Mercy Cheyech",
+    "content": "just explore deeper",
+    "date_posted": "February 2 2021"
+    },
+    {"title": "Get that power",
+    "author": "Grace Mwihaki",
+    "content": "Step by step through successful gain of power",
+    "date_posted": "June 13 2021"
+    }
+]
+
+# view functions
 def home(request):
     return render(request,"blog/home.html")
 
