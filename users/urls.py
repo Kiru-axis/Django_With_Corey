33392f09cc002bb37a89b727e2cc505e1fea2a5d
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-18 13:01:36 +0300

    configuring profile view to show pictures uploaded

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("register/",views.register,name = "users-register"),
@@ -8,3 +10,5 @@ urlpatterns = [
     path("logout/",auth_views.LogoutView.as_view(template_name = "users/logout.html"),name = "users-logout"),
     path("profile/",views.profile,name = "users-profile"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
